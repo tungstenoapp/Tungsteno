ecd6683cab46e443c017df6263ee412bec29bea5
JoseCarlosGarcia95 <hola@josecarlos.me>
2020-07-25 12:48:25 +0200

    Add support for variable definition.

---
+++ tsteno/builtin/misc/set.py
@@ -0,0 +1,21 @@
+from tsteno.atoms.module import Module, ModuleArg, ARG_FLAG_SPECIAL_CONTEXT
+
+
+class Set(Module):
+
+    def run(self, var, value, context):
+        evaluation = self.get_kernel().get_kext('eval')
+        evaluation.set_global_user_variable(str(var), value)
+
+        return True
+
+    def get_arguments(self):
+        return [
+            ModuleArg(), ModuleArg(), ModuleArg(ARG_FLAG_SPECIAL_CONTEXT)
+        ]
+
+    def run_test(self, test):
+        evaluation = self.get_kernel().get_kext('eval')
+
+        test.assertEqual(evaluation.evaluate_code(
+            'Set[a, 2]; Return[a+1]')[1], 3)
--- tsteno/kernel/kexts/evaluation.py
+++ tsteno/kernel/kexts/evaluation.py
@@ -25,16 +25,20 @@ CONTROL_FLOW_STATUS_R_STACK = 1
 
 
 class Context:
-    __slot__ = ['__control_flow']
+    __slot__ = ['__control_flow__', '__is_global__']
 
     def __init__(self):
-        self.__control_flow = CONTROL_FLOW_STATUS_P_STACK
+        self.__reset__()
 
     def set_control_flow(self, control_flow):
-        self.__control_flow = control_flow
+        self.__control_flow__ = control_flow
 
     def get_control_flow(self):
-        return self.__control_flow
+        return self.__control_flow__
+
+    def __reset__(self):
+        self.__is_global__ = True
+        self.__control_flow__ = CONTROL_FLOW_STATUS_P_STACK
 
 
 class Evaluation(KextBase):
@@ -114,14 +118,15 @@ class Evaluation(KextBase):
 
     def evaluate_parser_output(self, parser_output, context):
         if isinstance(parser_output, FunctionExpressionParserOutput):
-            module_definition = self.get_module_definition(parser_output.fname)
+            module_definition = self.get_module_definition(
+                parser_output.fname, context)
             return module_definition.eval(parser_output.arguments, context)
         elif isinstance(parser_output, StringParserOutput):
             return parser_output.value
         elif isinstance(parser_output, NumberExpressionParserOutput):
             return parser_output.value
         elif isinstance(parser_output, ExpressionParserOutput):
-            return self.get_variable_definition(parser_output.value)
+            return self.get_variable_definition(parser_output.value, context)
 
     def __load_builtin_module(self, path, module_def, log_kext=None):
         if log_kext is None:
@@ -153,7 +158,7 @@ class Evaluation(KextBase):
             LogLevel.DEBUG
         )
 
-    def get_module_definition(self, module):
+    def get_module_definition(self, module, context):
         if module in self.user_modules:
             return self.user_modules[module]
 
@@ -162,7 +167,10 @@ class Evaluation(KextBase):
 
         return self.builtin_modules[module]
 
-    def get_variable_definition(self, variable):
+    def set_global_user_variable(self, variable, value):
+        self.user_variables[variable] = value
+
+    def get_variable_definition(self, variable, context):
         if variable in self.user_variables:
             return self.user_variables[variable]
 
