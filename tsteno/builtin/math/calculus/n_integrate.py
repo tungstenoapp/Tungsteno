afe315ab61fc241dca168bb08c734340425b1bc6
JoseCarlosGarcia95 <hola@josecarlos.me>
2020-08-02 11:36:27 +0200

    Add n intgerate support.

---
+++ tsteno/builtin/math/calculus/n_integrate.py
@@ -0,0 +1,22 @@
+from sympy import integrate, parse_expr
+from tsteno.atoms.module import ModuleArg, Module
+from tsteno.atoms.module import ARG_FLAG_OPTIONAL, ARG_FLAG_ALL_NEXT
+
+
+class NIntegrate(Module):
+
+    def run(self, f, *variables):
+        return integrate(f, *variables).evalf()
+
+    def get_arguments(self):
+        return [
+            ModuleArg(),
+            ModuleArg(),
+            ModuleArg(ARG_FLAG_OPTIONAL | ARG_FLAG_ALL_NEXT)
+        ]
+
+    def run_test(self, test):
+        evaluation = self.get_kernel().get_kext('eval')
+
+        print(evaluation.evaluate_code(
+            'NIntegrate[x, {x, 0, 10}]'))
