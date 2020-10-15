0231af45ce6bd5d25fa982e0185fcc34a6df8987
JoseCarlosGarcia95 <hola@josecarlos.me>
2020-10-15 18:38:00 +0200

    Add lint testing.

--- tsteno/__init__.py
+++ tsteno/__init__.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
+"""
+# Tungsteno 
+
+"""
+
+""" Specify the current version of Tungsteno core """
 VERSION = 0.1
+""" Code name for current Tungsteno version """
 CODENAME = "Sophie"
+""" License information """
 COPYRIGHT = "Distributed under GPLv3 license 2020-2021"
