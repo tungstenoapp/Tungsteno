7d5923bee85aed9e346d3de1e163934e4ad21c94
JoseCarlosGarcia95 <hola@josecarlos.me>
2020-10-16 20:04:16 +0200

    Linting code & Improving docs.

--- tsteno/__init__.py
+++ tsteno/__init__.py
@@ -1,6 +1,8 @@
 """
 # Tungsteno
+Tugnsteno is a programming language intended to be 100 % compatible with Wolfram Language. It is designed to be lightweight, and easily extensible through modules, and module repositories.
 
+Designed to make users and contributors feel comfortable in a friendly and flexible environment.
 """
 
 """ Specify the current version of Tungsteno core """
