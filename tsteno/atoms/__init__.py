ea9a6b439c0a3b06a5a8904e00d690222dfaf643
JoseCarlosGarcia95 <josecarlos.garciaortega@alum.uca.es>
2020-10-19 17:35:39 +0200

    working

--- tsteno/atoms/__init__.py
+++ tsteno/atoms/__init__.py
@@ -1,3 +1,4 @@
 """
-Atoms represent the minimal information unit for Tunsgteno Programming Language.
+Atoms represent the minimal information unit for Tunsgteno Programming
+Language.
 """
--- tsteno/atoms/comparator.py
+++ tsteno/atoms/comparator.py
@@ -1,19 +1,41 @@
+"""
+Contains comparator operator atom.
+"""
 from .atoms import Atoms
 from functools import reduce
 
 
 class Comparator(Atoms):
+    """
+    Comparator represent a binary operation that compare two values.
+    """
     __slots__ = ('left', 'right', 'op')
 
     def __init__(self, left, right, op):
+        """
+        Parameters:
+            ** left ** - Left value to be compared.
+            ** right ** - Right value to be compared.
+            ** op ** -  Operator used to compare.
+        """
         self.left = left
         self.right = right
         self.op = op
 
     def eval(self):
+        """
+        Evaluate current comparator and generate an answer.
+
+        Return:
+            Return a boolean value if left & right are numeric.
+            Return a symbol if left or right are symbols.
+        """
         return reduce(self.op, (self.left, self.right))
 
     def __bool__(self):
+        """
+        Shortcut to eval()
+        """
         return self.eval()
 
     def __repr__(self):
